f10583e9f44d44c76d27bd2c82b279c352ceb773
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-09-09 08:32:33 -0700

    add the get people and get cost from the event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,17 @@
 public class Event {
 	private int people;
+	private float cost;
 
 	public Event(int numberOfpeople){
+		people = numberOfpeople;
+		cost = 0;
+	}
 
+	public int getPeople() {
+		return people;
+	}
+
+	public float getCost() {
+		return cost;
 	}
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -3,8 +3,22 @@ import static org.junit.Assert.*;
 
 public class EventTest {
   @Test
-  public void runGame_instantiatesCorrectly() {
+  public void runEvent_instantiatesCorrectly() {
     Event testEvent = new Event(5);
     assertEquals(true, testEvent instanceof Event);
+  }
+
+  @Test
+  public void runEvent_returnPeople() {
+  	Event testEvent = new Event(5);
+  	int numberOfpeople = testEvent.getPeople();
+  	assertEquals(5, numberOfpeople);
+  }
+
+  @Test
+  public void runEvent_returnCost() {
+  	Event testEvent = new Event(5);
+  	float cost = testEvent.getCost();
+    assertEquals(0.0, cost, 0.0f);
   }
 }
